42c23212761766d2f75e11cac9c27299d99b9561
konstantin shnaider <kostyash@bezeqint.net>
2024-07-14 00:06:06 +0300

    installed dev tools

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@ import { provideStore } from '@ngrx/store';
 import { AppComponent } from "./app/app.component";
 import { UserEffects } from './app/state/user.effects';
 import { userReducer } from './app/state/user.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideHttpClient(),
         provideStore(userReducer),
-        provideEffects(UserEffects),      
+        provideEffects(UserEffects),   
+        provideStoreDevtools({
+            maxAge: 25, // Retains last 25 states
+            logOnly: false, // Restrict extension to log-only mode
+          }),   
     ]
 });
