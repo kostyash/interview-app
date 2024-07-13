44afb020c635c5b64bf2fd77aa2d2cf70c6cd54b
konstantin shnaider <kostyash@bezeqint.net>
2024-07-14 00:53:42 +0300

    load users

--- src/app/components/users-list.component.ts
+++ src/app/components/users-list.component.ts
@@ -1,17 +1,37 @@
 import { AsyncPipe, NgFor } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, inject, Input, OnInit } from "@angular/core";
+import { User } from "../entities/contracts";
+import { select, Store } from "@ngrx/store";
+import { selectAllUsers } from "../state";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "app-users-list",
     template: `
         <h2>Following</h2>
         <ul>
+            <li *ngFor="let user of users$ | async">
+                {{user.username}}
+                {{user.totalPosts}}
+                {{user.likedPosts}}
 
         </ul>
     `,
     standalone: true,
     imports: [NgFor, AsyncPipe]
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+
+    store = inject(Store);
+
+    users$: Observable<User[]> = this.store.select(selectAllUsers)
+
+
+    ngOnInit(): void {
+
+
+    }
+
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { provideHttpClient } from "@angular/common/http";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
-import { AppComponent } from "./app/app.component";
-import { UserEffects } from './app/state/user.effects';
-import { userReducer } from './app/state/user.reducer';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { AppComponent } from "./app/app.component";
+import { reducers } from "./app/state";
+import { UserEffects } from './app/state/user.effects';
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideHttpClient(),
-        provideStore(userReducer),
+        provideStore(reducers),
         provideEffects(UserEffects),   
         provideStoreDevtools({
             maxAge: 25, // Retains last 25 states
